ae11e1c7664ffe9c0ce1f5d43511a4ae84163b18
Guilherme Müller <47581451+guilhermermuller@users.noreply.github.com>
2021-11-23 19:51:31 -0300

    Ultimas aulas

--- aula15/script.js
+++ aula15/script.js
@@ -13,6 +13,8 @@ function addNumber() {
         array.push(typedNumberValue)
         tab.appendChild(item)
     }
+    typedNumber.value = ''
+    typedNumber.focus()
 }
 
 function finalize() {
@@ -20,9 +22,29 @@ function finalize() {
     let max = Math.max(...array)
     let min = Math.min(...array)
     let sum = sumArray(array)
-    resultsDiv.innerHTML = `Ao todo, temos ${array.length} números cadastrados.<br> O maior valor informado foi ${max}. <br> O menor valor informado foi ${min}.`
+
+    if (array.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        resultsDiv.innerHTML = ''
+        resultsDiv.innerHTML += `<p>Ao todo, temos <strong>${array.length}</strong> números cadastrados.</p>` 
+        resultsDiv.innerHTML += `<p>O maior valor informado foi <strong>${max}</strong>.</p>`
+        resultsDiv.innerHTML += `<p>O menor valor informado foi <strong>${min}</strong>.</p>`
+        resultsDiv.innerHTML += `<p>Somando todos os valores temos <strong>${sum}</strong>.</p>`
+        resultsDiv.innerHTML += `<p>A média dos valores é <strong>${mediaArray(array)}</strong>.</p>`
+    }
 }
 
-function sumArray(array) {
-    resultado = 0
-} 
+function sumArray(temp) {
+    let resultado = 0
+    for(i = 0 ; i < temp.length ; i++) {
+        resultado += temp[i]
+    }
+    return resultado
+}
+
+function mediaArray(temp) {
+    let sum = sumArray(temp)
+    let length = temp.length
+    return sum / length
+}
